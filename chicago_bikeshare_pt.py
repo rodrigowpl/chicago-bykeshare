38f510e406e1e8b621d32efa36f4be61f6bf3469
Rodrigo Wippel <rodrigow1992@gmail.com>
2018-11-04 14:26:26 -0200

    feat: task 2

--- chicago_bikeshare_pt.py
+++ chicago_bikeshare_pt.py
@@ -34,7 +34,7 @@ data_list = data_list[1:]
 
 for index, line in enumerate(data_list):
     if index <= 20:
-        print('Line: {} - {}'.format(index, line))
+        print('Line {} - {}'.format(index, line))
 
 
 # Nós podemos acessar as features pelo índice
@@ -46,6 +46,9 @@ input("Aperte Enter para continuar...")
 
 print("\nTAREFA 2: Imprimindo o gênero das primeiras 20 amostras")
 
+for index, line in enumerate(data_list):
+    if index <= 20:
+        print('Gender {} - {}'.format(index, line[6]))
 
 # Ótimo! Nós podemos pegar as linhas(samples) iterando com um for, e as colunas(features) por índices.
 # Mas ainda é difícil pegar uma coluna em uma lista. Exemplo: Lista com todos os gêneros
